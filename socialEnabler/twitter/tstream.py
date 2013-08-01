eb3fe2f732655c2efa8c2e4c49c6dfb132c76376
evmorfia-b <evmorfiabiliri@gmail.com>
2013-08-01 10:40:27 +0300

    eleos

--- socialEnabler/twitter/tstream.py
+++ socialEnabler/twitter/tstream.py
@@ -59,6 +59,7 @@ class StreamListener(tweepy.StreamListener):
             pass
         else:
             #print 'Ran on_data'
+            print 'hey'
             #data_md5 = hashlib.md5(json.dumps(data, sort_keys=True)).hexdigest()
             #cbucket.set(data_md5,data)
             # print(json.loads(data))
